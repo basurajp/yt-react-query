5dd91af88d4bc53bf100b9ae45dfd21d3b14e7b9
Basuraj <basurajp135@gmail.com>
2024-04-20 23:56:16 +0530

    react query implemented

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,38 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getAllproducts } from "./Products";
+
 const Product = () => {
-    return <div>Product</div>;
+  const params = useParams();
+
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery({
+    queryKey: ["product", params.productId],
+    queryFn: getAllproducts,
+  });
+
+  const mutation = useMutation({
+    mutationFn: (newProduct) => {
+      return axios.put(`https://dummyjson.com/products/`, newProduct);
+    },
+  });
+
+  return (
+    <div>
+      <h1>the product is {params.productId}</h1>
+
+      <button
+        onClick={() => {
+          mutation.mutate({ title : "Do Laundry" });
+        }}
+      >
+        Create Todo
+      </button>
+    </div>
+  );
 };
 
 export default Product;
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import Products from './Products.jsx';
-import Product from './Product.jsx';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Products from "./Products.jsx";
+import Product from "./Product.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-    {
-        path: '/products',
-        element: <Products />,
-    },
-    {
-        path: '/products/:productId',
-        element: <Product />,
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/products/:productId",
+    element: <Product />,
+  },
 ]);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 10000,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+
+    </QueryClientProvider>
+  </React.StrictMode>
 );
